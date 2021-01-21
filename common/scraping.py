5656d718b867dce4bce9c1e6c207dcf2c0ce6f7a
hanqpark <official03x@gmail.com>
2021-01-21 20:30:31 +0900

    executable path

--- common/scraping.py
+++ common/scraping.py
@@ -9,7 +9,7 @@ def kartScraping():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(execution_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
     driver.get("https://kart.nexon.com/Kart/News/Patch/List.aspx?n4pageno=1")
 
     # 게시판 날짜별 패치 목록
